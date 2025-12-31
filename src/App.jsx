a9c7cb0e349ac37c2c97fb234847972e589728d0
samridhi8595-lab <samridhi8595@gmail.com>
2025-12-31 15:15:28 +0530

    code of stringify and object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const App = () => {
+   const user = {
+    username:'Sarthak',
+    age:18,
+    city:'Bhopal'
+   }
+   localStorage.setItem('user',JSON.stringify(user))
+  const user = JSON.parse(localStorage.getItem('user'))
+  console.log(user);
   return (
     <div>
       
